aef27064658b4ab286cecbdb78c6311ec7041cf6
Wajih <hass_wajih@hotmail.com>
2022-03-27 15:34:50 -0400

    Bootstrap-replica

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';// to be work should write npm install react-bootstrap bootstarp
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
